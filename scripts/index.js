34e9a6133f760a6def989941f1f893f6c526b93b
Ivan Dereza <dereza2012@yandex.ru>
2022-09-29 14:08:05 +0300

    fix: проектная работа после замечаний ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,44 @@
-const overlayEl = document.querySelector('.overlay');
-const formPopup = document.querySelector('.popup__form');
-const openEditButton = document.querySelector('.profile__edit-button'); //открытие Попапа
-const closePopupButton = overlayEl.querySelector('.popup__close-button'); //закрытие Попапа
-const profileName = document.querySelector('.profile__title'); //Имя пользователя
-const profileJob = document.querySelector('.profile__paragraph'); //Род деятельности
+//получили элементы 
+const popUp = document.querySelector('.popup');
+const popUpForm = document.querySelector('.popup__form')
+const openPopUpButton = document.querySelector('.profile__edit-button');
+const closePopUpButon = popUp.querySelector('.popup__close-button');
+const savePopUpButton = document.querySelector('.popup__save-button');
 
-const nameInput = document.querySelector('.popup__input-name'); // Воспользуйтесь инструментом .querySelector()
-const hobbieInput = document.querySelector('.popup__input-hobbie'); // Воспользуйтесь инструментом .querySelector()
-const popupSaveButton = document.querySelector('.popup__save-button');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputHobbie = document.querySelector('.popup__input_type_hobbie');
 
-openEditButton.addEventListener('click', () => {
-    overlayEl.classList.add('overlay__opened');
-    nameInput.value = profileName.textContent;
-    hobbieInput.value = profileJob.textContent;
-})
+const profileName = document.querySelector('.profile__title');
+const profileHobbie = document.querySelector('.profile__paragraph');
 
-closePopupButton.addEventListener('click', () => {
-    overlayEl.classList.remove('overlay__opened');
-})
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = hobbieInput.value;
+//навесили слушатель на клик
+function openPopUp() {
+    popUp.classList.add('popup__opened')
 }
 
+function closePopUp() {
+    popUp.classList.remove('popup__opened')
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileHobbie.textContent = inputHobbie.value;
+    closePopUp()
+
+}
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formPopup.addEventListener('submit', formSubmitHandler);
+popUpForm.addEventListener('submit', formSubmitHandler);
+
+openPopUpButton.addEventListener('click', () => {
+    inputName.value = profileName.textContent;
+    inputHobbie.value = profileHobbie.textContent;
+    openPopUp()
+})
+
+closePopUpButon.addEventListener('click', () => {
+    closePopUp()
+})
